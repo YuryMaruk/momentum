4e0012fa9d61e8366d1eda7ea4e023ddb9d1db92
Yury Maruk <maruk.dev@gmail.com>
2022-07-30 17:07:44 +0300

    feat: make return time of day

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,7 @@
 /* Show time */
 
-const timeClass = document.querySelector('.time');
-
 function showTime() {
+    const timeClass = document.querySelector('.time');
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     timeClass.textContent = currentTime;
@@ -13,17 +12,54 @@ showTime();
 
 /* Show date */
 
-const dateClass = document.querySelector('.date');
-
 function showDate() {
+    const dateClass = document.querySelector('.date');
     const date = new Date();
-    const options = {weekday: 'long', month: 'long', day: '2-digit'};
+    const options = { weekday: 'long', month: 'long', day: '2-digit' };
     const currentDate = date.toLocaleDateString('en-En', options);
     dateClass.textContent = currentDate;
     setInterval(showDate, 1000);
 }
 
 showDate();
+
+/* Show greeting */
+
+function showGreeting() {
+    const greeting = document.querySelector('.greeting');
+    const timeOfDay = getTimeOfDay();
+    
+    greeting.textContent = `Good ${timeOfDay}`;
+    setInterval(showGreeting, 1000);
+}
+
+function getTimeOfDay() {    /* This function returns the time of day */
+    const objOfDate = new Date();
+    const hours = objOfDate.getHours();
+    let timeOfDay = '';
+    let partOfDay = hours / 6;
+
+    switch (Math.floor(partOfDay)) {
+        case 0:
+            timeOfDay = 'night';
+            break;
+        case 1:
+            timeOfDay = 'morning';
+            break;
+        case 2:
+            timeOfDay = 'day';
+            break;
+        case 3:
+            timeOfDay = 'evening';
+            break; 
+    }
+
+    return timeOfDay;
+}
+
+showGreeting();
+
+
 
 
 
