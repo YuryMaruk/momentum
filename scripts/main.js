e45eef43e9b73328102bdacdc6b94e76749eef0d
Yury Maruk <maruk.dev@gmail.com>
2022-07-30 15:30:59 +0300

    feat: make show date

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,30 @@
-/* Time */
+/* Show time */
 
-const time = document.querySelector('.time');
-
+const timeClass = document.querySelector('.time');
 
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
-    time.textContent = currentTime;
+    timeClass.textContent = currentTime;
     setInterval(showTime, 1000);
 }
 
 showTime();
+
+/* Show date */
+
+const dateClass = document.querySelector('.date');
+
+function showDate() {
+    const date = new Date();
+    const options = {weekday: 'long', month: 'long', day: '2-digit'};
+    const currentDate = date.toLocaleDateString('en-En', options);
+    dateClass.textContent = currentDate;
+    setInterval(showDate, 1000);
+}
+
+showDate();
+
+
 
 
