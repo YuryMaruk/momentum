07137fbe3ae28eb4f2421fd816aed8507cdf3fcc
Yury Maruk <maruk.dev@gmail.com>
2022-08-08 16:25:46 +0300

    refactro: change display the playing song

--- scripts/playList.js
+++ scripts/playList.js
@@ -2,22 +2,22 @@ const playList = [
     {      
       title: 'Aqua Caelestis',
       src: '../audio/Aqua Caelestis.mp3',
-      duration: '00:40'
+      duration: '00:05'
     },  
     {      
       title: 'River Flows In You',
       src: '../audio/River Flows In You.mp3',
-      duration: '01:37'
+      duration: '00:05'
     },
     {      
       title: 'Ennio Morricone',
       src: '../audio/Ennio Morricone.mp3',
-      duration: '01:37'
+      duration: '00:05'
     },
     {      
       title: 'Summer Wind',
       src: '../audio/Summer Wind.mp3',
-      duration: '01:50'
+      duration: '00:05'
     }
   ]
   export default playList;
